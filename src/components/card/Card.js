2cc85d98538923fba761de8f4026616dde1535fd
andyholes <andyholesdev@gmail.com>
2023-06-27 17:32:31 -0300

    add games list

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,17 +1,19 @@
-import "./Card.css";
+import './Card.css'
 
-export default function Card({game}){
-    const { image, type, name, genre, platform, url, description } = game;
+function Card({game}){
+    const { imagen, tipoImagen, nombre, descripcion, url, idGenero, idPlataforma } = game;
     return(
         <div className="card">
-        <img src={"data:"+type+";base64,"+image} alt="" />
+        <img src={"data:"+tipoImagen+";base64,"+imagen} alt="" />
         <ul>
-            <li><h2>{name}</h2></li>
-            <li><p>{genre}</p></li>
-            <li><p>{platform}</p></li>
+            <li><h2>{nombre}</h2></li>
+            <li><p>{idGenero}</p></li>
+            <li><p>{idPlataforma}</p></li>
             <li><a href={url} target="blank">Sitio web</a></li>
-            <li><p>{description}</p></li>
+            <li><p>{descripcion}</p></li>
         </ul>
         </div>
     );
 }
+
+export default Card;
--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -1,27 +1,32 @@
 import './DashboardPage.css'
 import Card from '../../components/card/Card';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function DashboardPage() {
-   const [game, setGame] = useState(null);
+   const [games, setGames] = useState([]);
 
    useEffect(() => {
-      axios.get('https://api.example.com/game-data')
+      axios.get('http://localhost:3000/app/games')
         .then(response => {
-          setGame(response.data);
+          console.log('Received game data:', response.data);
+          setGames(response.data);
         })
         .catch(error => {
           console.error('Error fetching game data:', error);
         });
     }, []);
-  
+
+    console.log('Games:', games);
+
     return (
-      <div className="app">
-        {game && <Card game={game} />}
+      <div className="lista">
+        {games.map(game => (
+        <Card key={game.id} game={game} />
+      ))}
       </div>
     );
-    
+
   }
 
   
